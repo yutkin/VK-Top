68768647494de6a9a2848ab54541460d77a2cdb7
Dmitry Yutkin <yutkinn@gmail.com>
2016-01-09 00:29:14 +0300

    Bugs fixed. Improved perfomance.

--- const.py
+++ const.py
@@ -1,14 +1,38 @@
-import re
+"""
+This module defines the global constants.
+"""
 
-ACCESS_TOKEN = 'fa32f7e88fc054f31a43e43cf4fa0109e7a4da7196e7e855bc1522e8fef8bda7c575eab6884d8e272d1a1a941ece3'
+import re
+import textwrap
+
+
+APP_DESCRIPTION = textwrap.dedent('''
+    Sort posts of any public availabe page at VK.com.
+    Github: https://github.com/yutkin/VK-Top
+
+    Possible types of input URLs:
+    - https://vk.com/page_name
+    - http://vk.com/club12345
+    - public1234567
+    - id1234567
+    - event1234567
+    ''')
+
+LIKES = 'likes'
+REPOSTS = 'reposts'
+
+ACCESS_TOKEN = 'ff8faef07fe2666af8743cd4384fa595f85927d6c39dc4360e831a149d9f956710be447816dd857d2dee7'
 
 API_URL = 'https://api.vk.com/method/'
 API_V = 5.37
-TXT_ID_PTRN = r'(?:https?:\/\/)(?:vk.com\/(?!club|public|id|event))(?P<id>(?![_.])' \
-              r'(?!club|public|id|event)[a-z0-9_.]*[a-z][a-z0-9_.]*)'
 
-NUM_ID_PTRN = '^(?:https?:\/\/)?(?:vk.com\/)?(?P<type>club|public|id|event)' \
-              '(?P<id>\d+)$'
+INVALID_URL = ('{url} - invalid url address')
+NEGATIVE_ARGUMENT = ('{argument} - should be greater or equal to 0')
+MANY_REQUESTS = 6
+HIDDEN_WALL = 13
 
+
+TXT_ID_PTRN = r'(?:https?:\/\/)(?:vk.com\/(?!club|public|id|event))(?P<id>(?![_.])(?!club|public|id|event)[a-z0-9_.]*[a-z][a-z0-9_.]*)'
+NUM_ID_PTRN = r'^(?:https?:\/\/)?(?:vk.com\/)?(?P<type>club|public|id|event)(?P<id>\d+)$'
 TXT_ID_REGEXP = re.compile(TXT_ID_PTRN)
 NUM_ID_REGEXP = re.compile(NUM_ID_PTRN)
--- vktop.py
+++ vktop.py
@@ -1,32 +1,120 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Yutkin Dmitry
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import argparse
+import const
+from datetime import datetime, timedelta
 import json
 import sys
-from datetime import datetime, timedelta
-
 import requests
-import argparser
+import time
 
-import const
-
-class HiddenWall(Exception):
+class HiddenWall(BaseException):
     """ Dummy exception """
     pass
 
-class PageNotAvailable(Exception):
+
+class PageNotAvailable(BaseException):
     """ Dummy exception """
     pass
+
+
+def url_validator(arg):
+    """ Checks correctness of url argument """
+    arg = arg.lower()
+
+    # If url something like https://vk.com/textual_id
+    matched_txt_id = const.TXT_ID_REGEXP.match(arg)
+    if matched_txt_id:
+        url = matched_txt_id.groupdict()
+        url['type'] = 'symbolic'
+        return url
+
+    # If url something like https://vk.com/id123456
+    matched_numeric_id = const.NUM_ID_REGEXP.match(arg)
+    if matched_numeric_id:
+        return matched_numeric_id.groupdict()
+
+    raise argparse.ArgumentTypeError(
+            const.INVALID_URL.format(url=arg))
+
+
+def num_validator(arg):
+    """ Checks numbers on negativity """
+    num = int(arg)
+    if num >= 0:
+        return num
+    else:
+        raise argparse.ArgumentTypeError(
+                const.NEGATIVE_ARGUMENT.format(argument=arg))
+
+
+def parse_args():
+    """ Parses input arguments """
+    parser = argparse.ArgumentParser(description=const.APP_DESCRIPTION,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('url',
+                        action='store',
+                        default=None,
+                        help='target page',
+                        type=url_validator)
+
+    compar_key = parser.add_mutually_exclusive_group()
+
+    compar_key.add_argument('-l',
+                       '--likes',
+                       help='sort posts by likes (by default)',
+                       action='store_true',
+                       default=True)
+
+    compar_key.add_argument('-r',
+                       '--reposts',
+                       help='sort posts by reposts',
+                       action='store_true')
+
+    parser.add_argument('-t',
+                        '--top',
+                        help='number of showing posts. (10 by default)',
+                        default=10,
+                        type=num_validator)
+
+    parser.add_argument('-d',
+                        '--days',
+                        type=int,
+                        default=-1,
+                        help='last period (in days) for post processing. '
+                             '(all period by default)')
+
+    return parser.parse_args()
+
 
 def get_page_id(url):
-    """
-    Returns page numeric ID.
-
-    If url looks like https://vk.com/id12345 than return 12345,
-    else if url looks like https://vk.com/textual_name then
-    makes request to VK and gets numeric id from response.
-
-    """
+    """ Returns page's numeric ID """
     if url['type'] not in ['id', 'public', 'event', 'club']:
         params = {'screen_name': url['id']}
-        request = requests.get(const.API_URL + 'utils.resolveScreenName?', params=params)
+        request = requests.get(const.API_URL + 'utils.resolveScreenName?',
+                               params=params)
         response = json.loads(request.text)['response']
 
         if response:
@@ -42,82 +130,101 @@ def get_page_id(url):
         return -int(url['id'])
 
 
-def recieve_posts(page_id, last_days):
+def recieve_posts(page_id, last_days, reposts):
     """
-    Returns :received_posts: from :page_id: that were posted not earlier than
-    "today - :last_days:".
-
-    In endless loop make response. If response without errors (contains posts)
-    then in loop check received posts. If i-th post has appropriate date then
-    add it into :received_posts:.
-
+    Returns posts from :page_id: that were posted not earlier
+    than :last_days: ago
     """
-    received_posts = []
     deadline = datetime.now() - timedelta(days=last_days)
-    params = {'access_token': const.ACCESS_TOKEN, 'id': page_id}
+    unix_stamp = int(deadline.strftime("%s"))
+
+    if reposts:
+        compar_key = const.REPOSTS
+    else:
+        compar_key = const.LIKES
+
+    params = {'access_token': const.ACCESS_TOKEN,
+              'id': page_id,
+              'compar_key': compar_key,
+              'deadline':  unix_stamp if last_days != -1 else last_days
+              }
+    received_posts = []
 
     offset = 0
-    while True:
+    ONGOING = True
+    while ONGOING:
         params['offset'] = offset
         response = json.loads(requests.post(
-            const.API_URL + 'execute.getPosts?', params=params).text)
-
+            const.API_URL + 'execute.getPostsNew?', params=params).text)
 
         if 'error' in response:
-            # if many requests per second
-            if response['error']['error_code'] == 6:
+            error = response['error']['error_code']
+            if error == const.MANY_REQUESTS:
                 continue
-            # page has hidden wall
-            elif response['error']['error_code'] == 13:
+            if error == const.HIDDEN_WALL:
                 raise HiddenWall('Wall is closed for outside view')
-            else:
-                raise RuntimeError
+            print(response)
+            raise RuntimeError(response['error']['error_msg'])
 
-        # if received all posts from page
+        # Interrupt loop when all posts were received
         if not response['response']:
             break
 
         received_data = response['response']
         for chunk in received_data:
-            for i in range(chunk['chunk_size']):
-                post = {}
+            chunk_size = len(chunk['ids'])
+            for i in range(chunk_size):
+                post = dict()
                 post['date'] = datetime.fromtimestamp(chunk['dates'][i])
+                if chunk[compar_key][i] and (last_days == -1
+                    or post['date'].year >= deadline.year
+                    and post['date'].month >= deadline.month
+                    and post['date'].day >= deadline.day):
 
-                if last_days == -1 or post['date'].year >= deadline.year and \
-                                post['date'].month >= deadline.month and \
-                                post['date'].day >= deadline.day:
                     post['id'] = chunk['ids'][i]
-                    post['likes'] = chunk['likes'][i]
-                    post['reposts'] = chunk['reposts'][i]
-
-                    if post['likes'] != None and post['reposts'] != None:
-                        received_posts.append(post)
+                    post[compar_key] = chunk[compar_key][i]
+                    received_posts.append(post)
+            if 'stop' in chunk:
+                ONGOING = False
+                break
         offset += 1
 
     return received_posts
 
+
 def sort_posts(posts, reposts=False):
-    """ Sort posts by specified parameter. """
+    """ Sort posts by specified parameter """
     if reposts:
         return sorted(posts, key=lambda post: -post['reposts'])
     else:
         return sorted(posts, key=lambda post: -post['likes'])
 
+
 def main():
-    args = argparser.parse_args()
+    """  Main entry point for execution as a program """
+
+    init_point = time.time()
+    args = parse_args()
     try:
         page_id = get_page_id(args.url)
-        received_posts = sort_posts(recieve_posts(page_id, args.days), args.reposts)
+        print('Downloading posts. '
+              'This may take some time, be patient...')
+        received_posts = recieve_posts(page_id, args.days, args.reposts)
+        received_posts = sort_posts(received_posts, args.reposts)
     except (PageNotAvailable, HiddenWall) as error:
         print('{0}: error: {1}'.format(sys.argv[0], error))
+        return
+    except KeyboardInterrupt:
+        print('Exiting...')
         return
     except Exception:
         print('{0}: error: {1}'.format(sys.argv[0], 'Unknown error'))
         return
 
+
     post_count = len(received_posts)
     if args.top and post_count:
-        num_posts_for_showing = post_count-1 if args.top > post_count else args.top
+        num_posts_for_showing = post_count if args.top > post_count else args.top
     elif post_count:
         num_posts_for_showing = post_count
     else:
@@ -126,6 +233,7 @@ def main():
     max_url_width = len(str(max(received_posts, key=lambda x: x['id'])['id'])) + \
                     len('https://vk.com/wall_') + len(str(page_id))
 
+    print('Elapsed time: {:.2f} sec.'.format(time.time() - init_point))
     for i, post in enumerate(received_posts[:num_posts_for_showing]):
         link = 'https://vk.com/wall{0}_{1}'.format(page_id, post['id'])
         print('{num:>{num_width}} {url:<{url_width}} {date:<14} {type}: {count:,}'.format(
@@ -135,8 +243,8 @@ def main():
             url_width=max_url_width + 4,
             date=str(post['date'])[:10],
             type='Reposts' if args.reposts else 'Likes',
-            count=post['reposts'] if args.reposts else post['likes'],
-        ))
+            count=post['reposts'] if args.reposts else post['likes'])
+        )
 
 if __name__ == '__main__':
     main()
